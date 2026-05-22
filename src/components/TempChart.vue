d78ae0497eb6128f5610441fc61149a483dc18c1
Adele <adbaa007@hotmail.com>
2026-05-22 10:57:38 +0200

    Feilrettinger for kjøring på server

--- src/components/TempChart.vue
+++ src/components/TempChart.vue
@@ -1,46 +1,57 @@
 <script>
-
 </script>
 
 <template>
-    <div class="stats">
-      <div class="stats1">
-        <div class="headline">
-          <h4>Earned</h4>
-          <img src="/Users/adbaa007/project/dashboard-app/src/images/tjent.svg" alt="">
-        </div>
-        <div class="number">
-          <h2>739 000</h2>
-        </div>
+  <div class="stats">
+
+    <div class="stats1">
+      <div class="headline">
+        <h4>Earned</h4>
+        <img src="../images/tjent.svg" alt="">
       </div>
-      <div class="stats2">
-        <div class="headline">
-          <h4>Active customers</h4>
-          <img src="/Users/adbaa007/project/dashboard-app/src/images/kunder.svg" alt="">
-        </div>
-        <div class="number">
-          <h2>2693</h2>
-        </div>
-      </div>
-      <div class="stats3">
-        <div class="headline">
-          <h4>Solved cases</h4>
-          <img src="/Users/adbaa007/project/dashboard-app/src/images/problemer.svg" alt="">
-        </div>
-        <div class="number">
-          <h2>173</h2>
-        </div>
-      </div>
-      <div class="stats4">
-        <div class="headline">
-          <h4>Rating</h4>
-          <img src="/Users/adbaa007/project/dashboard-app/src/images/rating.svg" alt="">
-        </div>
-        <div class="number">
-          <h2>8.5</h2>
-        </div>
+
+      <div class="number">
+        <h2>739 000</h2>
       </div>
     </div>
+
+
+    <div class="stats2">
+      <div class="headline">
+        <h4>Active customers</h4>
+        <img src="../images/kunder.svg" alt="">
+      </div>
+
+      <div class="number">
+        <h2>1 284</h2>
+      </div>
+    </div>
+
+
+    <div class="stats3">
+      <div class="headline">
+        <h4>Open issues</h4>
+        <img src="../images/problemer.svg" alt="">
+      </div>
+
+      <div class="number">
+        <h2>37</h2>
+      </div>
+    </div>
+
+
+    <div class="stats4">
+      <div class="headline">
+        <h4>Customer rating</h4>
+        <img src="../images/rating.svg" alt="">
+      </div>
+
+      <div class="number">
+        <h2>4.8</h2>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <style scoped>
