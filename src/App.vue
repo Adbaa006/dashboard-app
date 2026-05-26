d51a5c36222bc51e975e3487ce25a7f365e21c28
Adele <adbaa007@hotmail.com>
2026-05-26 09:46:51 +0200

    Tester feilretting

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
-import Navbar from './components/Navbar.vue';
 import Menu from './components/Menu.vue';
 import Signering from './components/Signering.vue';
 </script>
 
 <template>
   <div class="grid-container">
-    <nav class="nav"><Navbar /></nav>
     <aside class="menu"><Menu /></aside>
 
     <main class="main">
